20d0c2dd7d0de41ef63d5c5179b8da5d71cf5de8
Sven Graziani <SGraziani@anexia-it.com>
2021-09-03 12:14:22 +0200

    update public-api, export tokens

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,8 +2,13 @@
  * Public API Surface of registry-loading-interceptor
  */
 export * from './lib/registry-loading-interceptor.module';
+export * from './lib/interceptors/registry-loading.interceptor';
+export * from './lib/tokens/request-id-generator-strategy';
+export * from './lib/tokens/request-filter-strategy';
+
 export * from './lib/interfaces/request-filter-strategy';
 export * from './lib/interfaces/request-id-generator-strategy';
+
 export * from './lib/strategies/no-request-filtering';
 export * from './lib/strategies/url-fragment-id-generator';
-export * from './lib/interceptors/registry-loading.interceptor';
+
