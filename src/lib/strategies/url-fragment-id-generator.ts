c9d97aeb2d0cc4cf585ef7fb21869ccbc24b4d7b
Sven Graziani <SGraziani@anexia-it.com>
2021-09-03 16:11:09 +0200

    restore static property

--- src/lib/strategies/url-fragment-id-generator.ts
+++ src/lib/strategies/url-fragment-id-generator.ts
@@ -3,11 +3,11 @@ import {HttpRequest} from '@angular/common/http';
 
 export class UrlFragmentIdGenerator implements RequestIdGeneratorStrategy {
 
-  getIdentifier(fragment: string): string {
+  public static getIdentifier(fragment: string): string {
     return String(fragment).replace(/[:\/&?]/g, '_').trim();
   }
 
   createId(request: HttpRequest<unknown>): string {
-    return this.getIdentifier(request.url);
+    return UrlFragmentIdGenerator.getIdentifier(request.url);
   }
 }
